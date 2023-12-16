5a4ef9bc69549ae20c07f735aced6813e4ec1db2
Paulo Scheuermann <paetzold44@gmail.com>
2023-12-16 00:45:34 -0400

    setup assets

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+const env = loadEnv(
+    'all',
+    process.cwd()
+);
+
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+            clientPort: parseInt(env.VITE_PORT),
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/assets/css/filament/admin/index.css',
+                'resources/assets/css/site/index.css',
+                'resources/assets/js/filament/admin/index.js',
+                'resources/assets/js/site/index.js'
+            ],
             refresh: true,
         }),
     ],
